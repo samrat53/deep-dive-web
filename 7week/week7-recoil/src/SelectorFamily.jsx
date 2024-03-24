3ce97aca40e6040c9239af9f915828fe82162994
samrat53 <samrat05hccs@gmail.com>
2024-03-25 04:36:28 +0530

    useRecoilStateLoadable

--- 7week/week7-recoil/src/SelectorFamily.jsx
+++ 7week/week7-recoil/src/SelectorFamily.jsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { RecoilRoot, useRecoilState, useRecoilValue } from "recoil";
+import {
+  RecoilRoot,
+  useRecoilState,
+  useRecoilStateLoadable,
+  useRecoilValue,
+} from "recoil";
 import { todosAtomsfamily } from "./store/selectorFamily";
 
 const SelectorFamily = () => {
@@ -7,22 +12,31 @@ const SelectorFamily = () => {
     <div>
       <h2>SelectorFamily</h2>
       <RecoilRoot>
-        <Todo id={1}/>
-        <Todo id={2}/>
-        <Todo id={1}/>
+        <Todo id={1} />
+        <Todo id={2} />
+        <Todo id={1} />
       </RecoilRoot>
     </div>
   );
 };
 
-const Todo=({id})=>{
-    const [todo,setTodo]=useRecoilState(todosAtomsfamily(id));
+const Todo = ({ id }) => {
+  // const [todo,setTodo]=useRecoilState(todosAtomsfamily(id)); // get directly
+  const [todo, setTodo] = useRecoilStateLoadable(todosAtomsfamily(id));
+  console.log(todo); // todo{state:..., contents:...}
+
+  if (todo.state === "loading") return <div>Loading boss...</div>;
+  else if (todo.state === "hasValue") {
     return (
-        <>
-        <p>{todo.title}</p>
-        <p>{todo.description}</p>
-        </>
-    )
-}
+      <>
+        <p>{todo.contents.title}</p>
+        <p>{todo.contents.description}</p>
+      </>
+    );
+  }
+  else if (todo.state === "hasError") {
+    return <div>Backend bkc kr diya</div>
+  }
+};
 
 export default SelectorFamily;
--- 7week/week7-recoil/src/store/selectorFamily.js
+++ 7week/week7-recoil/src/store/selectorFamily.js
@@ -5,6 +5,7 @@ export const todosAtomsfamily = atomFamily({
   default: selectorFamily({
     key:"todoSelectorFamily",
     get:(id) => async ({ get }) => {
+        await new Promise(r=>setTimeout(r,3000)); //artificial delay of 3 secs
         const res = await fetch(`https://sum-server.100xdevs.com/todo?id=${id}`);
         const todos=await res.json();
         console.log(todos.todo);
