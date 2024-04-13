3377cc64507203be90ff993d5a015afa4cfbbd84
samrat53 <samrat05hccs@gmail.com>
2024-04-14 01:52:47 +0530

    integrated signup endpoint

--- 8week/paytm-clone/frontend/src/pages/Signup.jsx
+++ 8week/paytm-clone/frontend/src/pages/Signup.jsx
@@ -8,18 +8,62 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 export const Signup = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [username, setusername] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div className="bg-slate-300 h-screen flex justify-center">
       <div className="flex flex-col justify-center">
         <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
           <Heading label={"Sign up"} />
           <SubHeading label={"Enter your infromation to create an account"} />
-          <InputBox placeholder="John" label={"First Name"} />
-          <InputBox placeholder="Doe" label={"Last Name"} />
-          <InputBox placeholder="samrat@gmail.com" label={"Email"} />
-          <InputBox placeholder="password1234" label={"Password"} />
+          <InputBox
+            placeholder="John"
+            onChange={(e) => {
+              setFirstName(e.target.value);
+            }}
+            label={"First Name"}
+          />
+          <InputBox
+            placeholder="Doe"
+            onChange={(e) => {
+              setLastName(e.target.value);
+            }}
+            label={"Last Name"}
+          />
+          <InputBox
+            placeholder="samrat@gmail.com"
+            onChange={(e) => {
+              setusername(e.target.value);
+            }}
+            label={"Email"}
+          />
+          <InputBox
+            placeholder="password1234"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            label={"Password"}
+          />
           <div className="pt-4">
-            <Button label={"Sign up"} />
+            <Button
+              onClick={async () => {
+                const response = await axios.post(
+                  "http://localhost:3000/api/v1/user/signup",
+                  {
+                    firstName,
+                    lastName,
+                    username,
+                    password,
+                  }
+                );
+                localStorage.setItem("token",response.data.token); //stores the session in browser
+                // localStorage.removeItem("token");
+              }}
+              label={"Sign up"}
+            />
           </div>
           <BottomWarning
             label={"Already have an account?"}
