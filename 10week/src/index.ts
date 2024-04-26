552bbd837ef944081b6ab0dd8a6fffb0ac2575b6
samrat53 <samrat05hccs@gmail.com>
2024-04-26 20:06:24 +0530

    sql relationship using foreiign keys and inner join

--- 10week/src/index.ts
+++ 10week/src/index.ts
@@ -58,6 +58,71 @@ const getUsers = async (email: string) => {
     console.log(`error finding the user`);
   }
 };
+
+const createAddressTable = async () => {
+  try {
+    await client.connect();
+    const query = `CREATE TABLE addresses(
+      id SERIAL PRIMARY KEY,
+      user_id INTEGER NOT NULL,
+      city VARCHAR(255) NOT NULL,
+      country VARCHAR(255) NOT NULL,
+      street VARCHAR(255) NOT NULL,
+      pincode VARCHAR(20),
+      createed_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+      FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE
+    );`;
+    const result = await client.query(query);
+    console.log(result);
+  } catch (error) {
+    console.log("error creating the table ", error);
+  }
+};
+
+const insertIntoAddresses = async (
+  user_id: number,
+  city: string,
+  country: string,
+  street: string,
+  pincode: number
+) => {
+  try {
+    await client.connect();
+    const values = [user_id, city, country, street, pincode];
+    const query = `INSERT INTO addresses (user_id,city,country,street,pincode) values ($1,$2,$3,$4,$5);`;
+    const result = await client.query(query, values);
+    console.log(result);
+  } catch (error) {
+    console.log(`error in values entry in addresses`, error);
+  }
+};
+
+const getUserDetailsWithAddresses = async (id: number) => {
+  try {
+    await client.connect();
+    const query = ` 
+    SELECT users.id, users.username, users.email, addresses.country, addresses.street, addresses.pincode
+    FROM users
+    JOIN addresses ON users.id=addresses.user_id
+    WHERE users.id= $1;
+    `;
+    //by defualt JOIN = INNER JOIN
+    const result = await client.query(query, [id]);
+
+    if (result.rows.length > 0) {
+      console.log(result.rows);
+      return result.rows;
+    } else {
+      console.log(`No users or address found for the given id`);
+      return null;
+    }
+  } catch (error) {
+    console.log(`error in find the matched details`, error);
+  }
+};
 // createUsersTable();
-// insertIntoTable("sam5", "sak@gmail.com", "passwordwerd");
-getUsers("sak@gmail.com");
+// insertIntoTable("sanam67", "snamm@gmail.com", "pashbh");
+// getUsers("sak@gmail.com");
+// createAddressTable();
+// insertIntoAddresses(4, "scl", "uk", "road-911009", 1099876543);
+getUserDetailsWithAddresses(3);
