d2c7c7eb13a3d3e9dd9c2b5527a248f182f38749
samrat53 <samrat05hccs@gmail.com>
2024-05-23 04:45:39 +0530

    cheap frontend builduo from coping

--- 12week/frontend-medium/src/components/Quote.tsx
+++ 12week/frontend-medium/src/components/Quote.tsx
@@ -1,22 +1,19 @@
+
 export const Quote = () => {
-  return (
-    <div className="bg-slate-200 h-screen flex justify-center flex-col">
-      <div className="flex justify-center">
-        <div className="max-w-lg">
-          <div className="text-3xl font-bold">
-            "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere
-            maxime quas, reprehenderit repudiandae minima, fugit, reiciendis in
-            sint quos quidem deleniti similique repellendus laboriosam officiis
-            natus exercitationem vero! Atque, obcaecati."
-          </div>
-          <div className="max-w-md text-lg text-left font-semibold mt-5">
-            Hello Sugar daddy
-          </div>
-          <div className="max-w-md text-left text-sm font-light text-slate-400">
-            CEO | Corpus Callosum
-          </div>
+    return <div className="bg-slate-200 h-screen flex justify-center flex-col">
+        <div className="flex justify-center">
+            <div className="max-w-lg">
+                <div className="text-3xl font-bold">
+                    "The customer support I received was exceptional. The support team went above and beyond to address my concerns"
+                </div>
+                <div className="max-w-md text-xl font-semibold text-left mt-4">
+                    Julies Winfield
+                </div>
+                <div className="max-w-md text-sm font-light text-slate-400">
+                    CEO | Acme corp
+                </div>
+            </div>
         </div>
-      </div>
+        
     </div>
-  );
-};
+}
