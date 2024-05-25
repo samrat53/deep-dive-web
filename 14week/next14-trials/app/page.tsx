2cf3e7855d2bd78b9e68ac53074c7cbb9bb4c4e1
samrat53 <samrat05hccs@gmail.com>
2024-05-25 23:15:59 +0530

    testing aceternity

--- 14week/next14-trials/app/page.tsx
+++ 14week/next14-trials/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import { TabsDemo } from "@/components/tabsDemo";
 
 export default function Home() {
   return (
     <div>
-      Hello World
+      <TabsDemo/>
     </div>
   );
 }
