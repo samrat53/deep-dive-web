2b1775c16ea232a795dfbb0d21367297679948da
samrat53 <samrat05hccs@gmail.com>
2024-06-12 03:32:27 +0530

    messages: turborepos and backend imports, tsup and esbuild

--- 16week-live/week-16-1/apps/backend/src/index.ts
+++ 16week-live/week-16-1/apps/backend/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 const app=express();
 import {BACKEND_URL} from "@repo/common/config"
 console.log(BACKEND_URL);
+// to log into the console i.e to resolve inputs from a differerent library , use esbuild or tsup, npx esbuild ./src/index.ts --bundle --platform=node --outfile=dist/index.js
 
 app.get("/",(req,res)=>{
     res.json({
