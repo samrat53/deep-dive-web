86ddcbc97dac53417e7f65017b456ffc482240bf
samrat53 <samrat05hccs@gmail.com>
2025-01-19 00:30:22 +0530

    populating redis as queue

---
+++ 19week/redis/express-server/src/index.ts
@@ -0,0 +1,30 @@
+import express from "express"
+import { createClient } from "redis"
+
+const app = express();
+const client = createClient();
+client.on('error', (err) => console.log(`Redis client error: ${err}`));
+app.use(express.json());
+
+const startServer = async() => {
+    try {
+        await client.connect();
+        console.log("client connected");
+
+        app.listen(3000, () => console.log("server on port 3000"));
+    } catch (error) {
+        console.log(error);
+    }
+}
+startServer();
+
+app.post('/submit', async(req, res) => {
+    const {problemid, code, language} = req.body;
+    // push to a db also
+    try {
+        await client.lPush("problems", JSON.stringify({problemid, code, language}));
+        res.status(200).json({msg: "submission received and stored"});
+    } catch (error) {
+        console.log(error);
+    }
+})
